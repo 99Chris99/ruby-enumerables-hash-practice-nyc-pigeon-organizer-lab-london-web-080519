370e02cab9b11dc40a319c2f16704efe09dbaa8b
Chris Hodge <chrishodge87@gmail.com>
2019-07-22 09:19:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,8 @@ def nyc_pigeon_organizer(data)
 output = {}
 
     data.each do |attri, val|
-        val.each do |name, array|
-            array.each do |pigeon|
+    val.each do |name, array|
+    array.each do |pigeon|
 
             if !output[pigeon]
                     output[pigeon] =
